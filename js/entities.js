7f3f6740640f00087ef3f5f0eae4f7ed55b26d18
JASW-1989 <jasontomai@gmail.com>
2026-01-06 08:10:05 +0800

    Update entities.js

--- js/entities.js
+++ js/entities.js
@@ -1,13 +1,8 @@
 /**
- * js/entities.js
- * 使用物件導向 (OOP) 定義遊戲中所有會動的實體
+ * js/entities.js - 物件導向實體定義
  */
-
 import { Utils } from './utils.js';
 
-/**
- * 基礎實體類別
- */
 class BaseEntity {
     constructor(x, y, icon) {
         this.x = x;
@@ -17,125 +12,83 @@ class BaseEntity {
     }
 }
 
-/**
- * 敵人 (Enemy) 類別
- */
 export class Enemy extends BaseEntity {
     constructor(data, path, scaling, speedGrowth, wave) {
-        // 敵人永遠從路徑起點生成
         super(path[0].x, path[0].y, data.icon);
+        this.data = data; 
         this.name = data.name;
-        this.maxHp = data.hp * scaling;
-        this.currentHp = this.maxHp;
+        this.hp = data.hp * scaling;
+        this.currentHp = this.hp;
         this.speed = data.speed * (1 + (wave * speedGrowth));
-        this.pi = 0; // 當前路徑節點索引
+        this.pi = 0;
         this.isBoss = data.isBoss || false;
-        this.blocker = null; // 記錄當前攔截此敵人的女神
+        this.blocker = null;
     }
 
-    /**
-     * 更新怪物移動邏輯
-     */
     update(path, balance, onLeak) {
-        if (this.currentHp <= 0) {
-            this.dead = true;
-            return;
-        }
-
-        // 如果被近戰女神攔截，且女神還活著，則停止移動
-        if (this.blocker && this.blocker.currentHp > 0) {
-            return; 
-        } else {
-            this.blocker = null;
-        }
-
-        const nextPoint = path[this.pi + 1];
-        if (nextPoint) {
-            const dx = nextPoint.x - this.x;
-            const dy = nextPoint.y - this.y;
-            const d = Math.hypot(dx, dy);
-
-            if (d < this.speed) {
-                this.pi++; // 到達節點，轉向下一個
-            } else {
-                this.x += (dx / d) * this.speed;
-                this.y += (dy / d) * this.speed;
+        if (this.currentHp <= 0) { this.dead = true; return; }
+        if (this.blocker && this.blocker.currentHp > 0) return; // 被攔截中
+        
+        const next = path[this.pi + 1];
+        if (next) {
+            const dx = next.x - this.x, dy = next.y - this.y, d = Math.hypot(dx, dy);
+            if (d < this.speed) this.pi++;
+            else { 
+                this.x += (dx / d) * this.speed; 
+                this.y += (dy / d) * this.speed; 
             }
         } else {
-            // 到達終點 (漏怪)
-            const dmg = this.isBoss ? balance.damage_system.boss_leak_penalty : balance.damage_system.normal_leak_penalty;
-            onLeak(dmg);
+            // 到達終點
+            onLeak(this.isBoss ? balance.damage_system.boss_leak_penalty : balance.damage_system.normal_leak_penalty);
             this.dead = true;
         }
     }
 }
 
-/**
- * 女神單位 (Unit) 類別
- */
 export class Unit extends BaseEntity {
     constructor(data, x, y) {
         super(x, y, data.icon);
+        this.config = data; // 保存原始配置供 UI 讀取 upgrades
         this.id = data.id;
         this.name = data.name;
-        this.type = data.type; // R_PHYS, M_TANK, R_MAGIC 等
+        this.type = data.type;
         this.range = data.range;
         this.damage = data.damage;
         this.cooldown = data.cooldown;
         this.color = data.color || "#ff66aa";
-        
         this.maxHp = data.hp || 1500;
         this.currentHp = this.maxHp;
-        this.lastShotFrame = 0;
+        this.lastShot = 0;
         this.level = 1;
     }
 
-    /**
-     * 判定是否可以射擊
-     */
-    tryFire(enemies, currentFrame, onFire) {
-        // 冷卻檢查
-        if (currentFrame - this.lastShotFrame < this.cooldown) return;
-
-        // 尋找射程內最靠近終點的敵人
-        const targets = enemies.filter(e => 
-            !e.dead && 
-            Utils.getDist(this, e) < this.range
-        );
-
+    tryFire(enemies, frame, onFire) {
+        if (frame - this.lastShot < this.cooldown) return;
+        const targets = enemies.filter(e => !e.dead && Utils.getDist(this, e) < this.range);
         if (targets.length > 0) {
-            // 排序：路徑索引(pi)越大、距離下一個點越近的優先
-            targets.sort((a, b) => b.pi - a.pi);
-            
+            targets.sort((a, b) => b.pi - a.pi); // 優先攻擊最靠近終點的敵人
             onFire(this, targets[0]);
-            this.lastShotFrame = currentFrame;
+            this.lastShot = frame;
         }
     }
 }
 
-/**
- * 投射物 (Projectile) 類別
- */
 export class Projectile extends BaseEntity {
-    constructor(sourceX, sourceY, target, damage, color) {
-        super(sourceX, sourceY, "");
+    constructor(sourceUnit, target) {
+        super(sourceUnit.x, sourceUnit.y, "");
         this.target = target;
-        this.damage = damage;
-        this.color = color;
+        this.damage = sourceUnit.damage;
+        this.color = sourceUnit.color;
         this.speed = 45;
     }
 
     update(onHit) {
-        if (this.target.dead || this.target.currentHp <= 0) {
-            this.dead = true;
-            return;
-        }
-
         const d = Utils.getDist(this, this.target);
-        if (d < this.speed) {
-            // 命中
-            this.target.currentHp -= this.damage;
-            onHit(this.target);
+        if (d < this.speed || this.target.dead) {
+            if (!this.target.dead) {
+                this.target.currentHp -= this.damage;
+                if (this.target.currentHp <= 0) onHit(this.target);
+            }
             this.dead = true;
         } else {
             this.x += (this.target.x - this.x) / d * this.speed;
