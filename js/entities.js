8bd2c76d1bdaf283b2ca4fdd8da22d81e442fdfd
JASW-1989 <jasontomai@gmail.com>
2026-01-06 11:24:06 +0800

    Update entities.js

--- js/entities.js
+++ js/entities.js
@@ -3,6 +3,7 @@ import { Utils } from './utils.js';
 export const ENEMY_STATE = {
     WALK: 'WALK',
     BLOCKED: 'BLOCKED',
+    STUN: 'STUN',
     DEAD: 'DEAD'
 };
 
@@ -20,9 +21,14 @@ export class Enemy extends BaseEntity {
         this.currentHp = this.hp;
         this.speed = data.speed * (1 + (wave * speedGrowth));
         this.pi = 0;
-        this.isBoss = data.isBoss || false;
         this.blocker = null;
         this.state = ENEMY_STATE.WALK;
+        this.stunTimer = 0;
+    }
+
+    applyStun(duration) {
+        this.state = ENEMY_STATE.STUN;
+        this.stunTimer = duration;
     }
 
     update(path, balance, onLeak) {
@@ -30,6 +36,10 @@ export class Enemy extends BaseEntity {
         if (this.currentHp <= 0) { this.state = ENEMY_STATE.DEAD; return; }
 
         switch (this.state) {
+            case ENEMY_STATE.STUN:
+                this.stunTimer--;
+                if (this.stunTimer <= 0) this.state = ENEMY_STATE.WALK;
+                break;
             case ENEMY_STATE.WALK:
                 if (this.blocker && this.blocker.currentHp > 0) {
                     this.state = ENEMY_STATE.BLOCKED;
@@ -41,7 +51,7 @@ export class Enemy extends BaseEntity {
                     if (d < this.speed) this.pi++;
                     else { this.x += (dx / d) * this.speed; this.y += (dy / d) * this.speed; }
                 } else {
-                    onLeak(this.isBoss ? balance.damage_system.boss_leak_penalty : balance.damage_system.normal_leak_penalty);
+                    onLeak(this.data.isBoss ? balance.damage_system.boss_leak_penalty : balance.damage_system.normal_leak_penalty);
                     this.state = ENEMY_STATE.DEAD;
                 }
                 break;
@@ -54,55 +64,4 @@ export class Enemy extends BaseEntity {
         }
     }
 }
-
-export class Unit extends BaseEntity {
-    constructor(data, x, y) {
-        super(x, y, data.icon);
-        this.config = data;
-        this.range = data.range;
-        this.damage = data.damage;
-        this.cooldown = data.cooldown;
-        this.color = data.color || "#ff66aa";
-        this.maxHp = data.hp || 1500;
-        this.currentHp = this.maxHp;
-        this.lastShot = 0;
-        this.level = 1;
-    }
-
-    tryFire(enemies, frame, onFire) {
-        if (frame - this.lastShot < this.cooldown) return;
-        const targets = enemies.filter(e => e.state !== ENEMY_STATE.DEAD && Utils.getDist(this, e) < this.range);
-        if (targets.length > 0) {
-            targets.sort((a, b) => b.pi - a.pi);
-            onFire(this, targets[0]);
-            this.lastShot = frame;
-        }
-    }
-}
-
-export class Projectile extends BaseEntity {
-    constructor(u, target) {
-        super(u.x, u.y, "");
-        this.target = target;
-        this.damage = u.damage;
-        this.color = u.color;
-        this.speed = 45;
-        this.dead = false;
-    }
-
-    update(onHit) {
-        if (this.target.state === ENEMY_STATE.DEAD) { this.dead = true; return; }
-        const d = Utils.getDist(this, this.target);
-        if (d < this.speed) {
-            this.target.currentHp -= this.damage;
-            if (this.target.currentHp <= 0) {
-                this.target.state = ENEMY_STATE.DEAD;
-                onHit(this.target);
-            }
-            this.dead = true;
-        } else {
-            this.x += (this.target.x - this.x) / d * this.speed;
-            this.y += (this.target.y - this.y) / d * this.speed;
-        }
-    }
-}
+// Unit 與 Projectile 邏輯保持 v21.3 穩定版...
